8a65f89db1893ab31b4a7f1f6e27b583e966d09c
RedesCristian <149571424+RedesCristian@users.noreply.github.com>
2024-06-07 20:29:21 +0000

    Deploying to gh-pages from @ RedesCristian/team6@39bbab6b47790843dcb9a9710c7b9520cd00818f 🚀

--- index.9610d826.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.getElementById("theme-toggle-btn"),e=document.body;t.addEventListener("click",(function(){e.classList.contains("light-mode")?(e.classList.remove("light-mode"),e.classList.add("dark-mode"),t.textContent="Light"):(e.classList.remove("dark-mode"),e.classList.add("light-mode"),t.textContent="Dark")}))}();
-//# sourceMappingURL=index.9610d826.js.map
--- index.b9d977d3.js
+++
@@ -1,2 +0,0 @@
-const t=document.getElementById("theme-toggle-btn"),e=document.body;t.addEventListener("click",(()=>{e.classList.contains("light-mode")?(e.classList.remove("light-mode"),e.classList.add("dark-mode"),t.textContent="Light"):(e.classList.remove("dark-mode"),e.classList.add("light-mode"),t.textContent="Dark")}));
-//# sourceMappingURL=index.b9d977d3.js.map
