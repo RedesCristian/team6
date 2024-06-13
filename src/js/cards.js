17a8eb05638555a002581aa3d036a6714859d6c3
RedesCristian <149571424+RedesCristian@users.noreply.github.com>
2024-06-13 20:15:01 +0300

    update cards.js

--- src/js/cards.js
+++ src/js/cards.js
@@ -10,10 +10,12 @@ async function fetchPosters() {
   try {
     const response = await axios.get(URL);
     const { results } = response.data;
-    console.log(response.data);
     renderGallery(results);
   } catch (error) {
     console.error(error);
+    Notiflix.Notify.failure(
+      'Failed to fetch popular movies. Please try again later.'
+    );
   }
 }
 
@@ -77,111 +79,111 @@ function renderGallery(posters) {
 }
 
 async function openModal(id) {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=904cc36a32d92a605c14a646cc21fc67`
-  );
-  const movieData = await response.json();
-  const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
+  const API_KEY = '904cc36a32d92a605c14a646cc21fc67';
+  const URL = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
 
-  const isWatched = checkMovieInLibrary('watched', id);
-  const isQueued = checkMovieInLibrary('queue', id);
+  try {
+    const response = await fetch(URL);
+    const movieData = await response.json();
+    const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-  const modalContent = `
-    <div class="cards-modal-content">
-      <span class="cards-modal-close-button">&times;</span>
-      <img class="cards-modal-poster-card" src="${
-        BASE_IMG_URL + movieData.poster_path
-      }" alt="${movieData.title}" loading="lazy" />
-      <div class="cards-modal-div-info">
-        <h2 class="cards-modal-title">${movieData.title}</h2>
-        <div class="cards-modal-details">
-          <span class="label">Vote / Votes</span>
-          <span class="value">${movieData.vote_average} / ${
-    movieData.vote_count
-  }</span>
-        </div>
-        <div class="cards-modal-details">
-          <span class="label">Popularity</span>
-          <span class="value">${movieData.popularity}</span>
-        </div>
-        <div class="cards-modal-details">
-          <span class="label">Original Title</span>
-          <span class="value">${movieData.original_title}</span>
-        </div>
-        <div class="cards-modal-details">
-          <span class="label">Genre</span>
-          <span class="value">${movieData.genres
-            .map(genre => genre.name)
-            .join(', ')}</span>
-        </div>
-        <div class="cards-modal-about">
-          <span class="label">About</span>
-          <span class="value">${movieData.overview}</span>
-        </div>
-        <div class="cards-modal-buttons">
-          <button id="toggle-watched" class="cards-modal-button watch">${
-            isWatched ? 'REMOVE FROM WATCHED' : 'ADD TO WATCHED'
-          }</button>
-          <button id="toggle-queue" class="cards-modal-button queue">${
-            isQueued ? 'REMOVE FROM QUEUE' : 'ADD TO QUEUE'
-          }</button>
+    const modalContent = `
+      <div class="cards-modal-content">
+        <span class="cards-modal-close-button">&times;</span>
+        <img class="cards-modal-poster-card" src="${
+          BASE_IMG_URL + movieData.poster_path
+        }" alt="${movieData.title}" loading="lazy" />
+        <div class="cards-modal-div-info">
+          <h2 class="cards-modal-title">${movieData.title}</h2>
+          <div class="cards-modal-details">
+            <span class="label">Vote / Votes</span>
+            <span class="value">${movieData.vote_average} / ${
+      movieData.vote_count
+    }</span>
+          </div>
+          <div class="cards-modal-details">
+            <span class="label">Popularity</span>
+            <span class="value">${movieData.popularity}</span>
+          </div>
+          <div class="cards-modal-details">
+            <span class="label">Original Title</span>
+            <span class="value">${movieData.original_title}</span>
+          </div>
+          <div class="cards-modal-details">
+            <span class="label">Genre</span>
+            <span class="value">${movieData.genres
+              .map(genre => genre.name)
+              .join(', ')}</span>
+          </div>
+          <div class="cards-modal-about">
+            <span class="label">About</span>
+            <span class="value">${movieData.overview}</span>
+          </div>
+          <div class="cards-modal-buttons">
+            <button id="toggle-watched" class="cards-modal-button watch">Loading...</button>
+            <button id="toggle-queue" class="cards-modal-button queue">Loading...</button>
+          </div>
         </div>
       </div>
-    </div>
-  `;
+    `;
 
-  const modal = document.createElement('div');
-  modal.classList.add('cards-modal-modal');
-  modal.innerHTML = modalContent;
-  document.body.appendChild(modal);
-  modal.style.display = 'block';
+    const modal = document.createElement('div');
+    modal.classList.add('cards-modal-modal');
+    modal.innerHTML = modalContent;
+    document.body.appendChild(modal);
+    modal.style.display = 'block';
 
-  function closeModal() {
-    modal.style.display = 'none';
-    document.body.removeChild(modal);
+    function closeModal() {
+      modal.style.display = 'none';
+      document.body.removeChild(modal);
+    }
+
+    modal
+      .querySelector('.cards-modal-close-button')
+      .addEventListener('click', closeModal);
+    window.addEventListener('click', event => {
+      if (event.target === modal) {
+        closeModal();
+      }
+    });
+    window.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        closeModal();
+      }
+    });
+
+    // Actualizează starea butoanelor în funcție de prezența filmului în biblioteca locală
+    updateButtonState('watched', movieData.id, 'toggle-watched');
+    updateButtonState('queue', movieData.id, 'toggle-queue');
+
+    document.getElementById('toggle-watched').addEventListener('click', () => {
+      toggleMovieInLibrary('watched', movieData);
+      updateButtonState('watched', movieData.id, 'toggle-watched');
+    });
+
+    document.getElementById('toggle-queue').addEventListener('click', () => {
+      toggleMovieInLibrary('queue', movieData);
+      updateButtonState('queue', movieData.id, 'toggle-queue');
+    });
+  } catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure(
+      'Failed to fetch movie details. Please try again later.'
+    );
   }
-
-  modal
-    .querySelector('.cards-modal-close-button')
-    .addEventListener('click', closeModal);
-  window.addEventListener('click', event => {
-    if (event.target === modal) {
-      closeModal();
-    }
-  });
-
-  window.addEventListener('keydown', event => {
-    if (event.key === 'Escape') {
-      closeModal();
-    }
-  });
-
-  document.getElementById('toggle-watched').addEventListener('click', () => {
-    toggleMovieInLibrary('watched', movieData);
-  });
-
-  document.getElementById('toggle-queue').addEventListener('click', () => {
-    toggleMovieInLibrary('queue', movieData);
-  });
 }
 
 function toggleMovieInLibrary(section, movie) {
   let movies = JSON.parse(localStorage.getItem(section)) || [];
   const movieIndex = movies.findIndex(m => m.id === movie.id);
 
-  const button = document.getElementById(`toggle-${section}`);
-
   if (movieIndex === -1) {
     movies.push(movie);
-    button.textContent = `REMOVE FROM ${section.toUpperCase()}`;
-    button.classList.add('remove');
     Notiflix.Notify.info(
       `The movie has been added to ${section.toUpperCase()}`
     );
   } else {
     movies.splice(movieIndex, 1);
-    button.textContent = `ADD TO ${section.toUpperCase()}`;
-    button.classList.remove('remove');
     Notiflix.Notify.info(
       `The movie has been removed from ${section.toUpperCase()}`
     );
@@ -193,4 +195,16 @@ function toggleMovieInLibrary(section, movie) {
 function checkMovieInLibrary(section, id) {
   const movies = JSON.parse(localStorage.getItem(section)) || [];
   return movies.some(movie => movie.id === id);
+}
+
+function updateButtonState(section, id, buttonId) {
+  const isInLibrary = checkMovieInLibrary(section, id);
+  const button = document.getElementById(buttonId);
+  if (isInLibrary) {
+    button.textContent = `REMOVE FROM ${section.toUpperCase()}`;
+    button.classList.add('remove');
+  } else {
+    button.textContent = `ADD TO ${section.toUpperCase()}`;
+    button.classList.remove('remove');
+  }
 }
