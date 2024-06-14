a71675f2ade18c9af4ac0cf8d41f2baee33e414c
lewypopescu <popesculewy@gmail.com>
2024-06-14 22:57:46 +0200

    done final

--- src/my-library.js
+++ src/my-library.js
@@ -8,9 +8,9 @@ import Notiflix from 'notiflix';
 const galleryLibrary = document.querySelector('.galleryLibrary');
 
 if (
-  window.location.pathname ===
-    'https://redescristian.github.io/team6/my-library.html' ||
-  window.location.pathname === '/my-library.html'
+  window.location.pathname === '/my-library.html' ||
+  window.location.pathname === '/team6/my-library.html' ||
+  window.location.pathname.includes('/team6/my-library.html')
 ) {
   displayMovies('watched');
 }
