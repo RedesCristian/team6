3531e57c69548ceb75ffb4e2822bc9bd64401876
SecaraAdrian <secaforce@gmail.com>
2024-06-16 16:20:06 +0300

    update dark-mode button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 
 import './js/footer.js';
 import './js/cards';
+<<<<<<< Updated upstream
 import './js/dark-mode';
+=======
+import './js/dark-mode.js';
+import './js/spinner.js';
+import './js/search.js';
+>>>>>>> Stashed changes
--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,8 +1,16 @@
-//extra js pentru link home si index
-// document.querySelector('home-btn').addEventListener('click', function () {
-//   window.location.href = window.location.origin + '/team6/index.html';
-// });
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
 
-// document.querySelector('library-btn').addEventListener('click', function () {
-//   window.location.href = window.location.origin + '/team6/my-library.html';
-// });
+
+function toggleTheme() {
+  if (body.getAttribute('data-theme') === 'dark') {
+    body.setAttribute('data-theme', 'light');
+    body.style.backgroundColor = '#f0f0f0'; 
+  } else {
+    body.setAttribute('data-theme', 'dark');
+    body.style.backgroundColor = '#333'; 
+  }
+}
+
+
+themeToggle.addEventListener('click', toggleTheme);
