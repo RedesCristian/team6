655ef548e0ebcb32d556c0aedc6bc7c29f368fe2
roxananecsoi <radu.roxana07@gmail.com>
2024-06-17 13:28:20 +0300

    modificare header responsive

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,16 +1,14 @@
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
-
 function toggleTheme() {
   if (body.getAttribute('data-theme') === 'dark') {
     body.setAttribute('data-theme', 'light');
-    body.style.backgroundColor = '#f0f0f0'; 
+    body.style.backgroundColor = '#f0f0f0';
   } else {
     body.setAttribute('data-theme', 'dark');
-    body.style.backgroundColor = '#333'; 
+    body.style.backgroundColor = '#333';
   }
 }
-
 
 themeToggle.addEventListener('click', toggleTheme);
